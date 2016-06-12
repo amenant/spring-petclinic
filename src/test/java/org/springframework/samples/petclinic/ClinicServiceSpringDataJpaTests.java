30888b7a004a163f182d14f32f151204f9832e82
amenant <alan.menant@zenika.com>
2016-06-12 14:58:51 +0800

    Implement unit test for Integer
    Implement integration test for ClinicService
    Use AssertJ

---
+++ src/test/java/org/springframework/samples/petclinic/ClinicServiceSpringDataJpaTests.java
@@ -0,0 +1,32 @@
+package org.springframework.samples.petclinic;
+
+import java.util.Collection;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.samples.petclinic.model.Owner;
+import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(BootApplication.class)
+@ActiveProfiles("test")
+public class ClinicServiceSpringDataJpaTests {
+
+	@Autowired
+    protected ClinicService clinicService;
+	
+	@Test
+    public void findOwners() {
+        Collection<Owner> owners = this.clinicService.findOwnerByLastName("Davis");
+        Assertions.assertThat(owners.size()).isEqualTo(2);
+        
+        owners = this.clinicService.findOwnerByLastName("Daviss");
+        Assertions.assertThat(owners.size()).isEqualTo(0);
+    }
+	
+}
