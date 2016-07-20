c589effaf20b74da23e58075a578959636b2555e
Lim Han <lim.han@gmail.com>
2016-07-20 10:09:44 +0800

    LH :: Fixed functional-test run

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,32 +2,18 @@ module.exports = function(grunt) {
     grunt.initConfig({
       protractor: {
         options: {
-          configFile: "src/test/resources/protractor.conf.js", // Default config file
-          keepAlive: true, // If false, the grunt process stops when the test fails.
+          configFile: "node_modules/protractor/example/conf.js", // Default config file
+          keepAlive: false, // If false, the grunt process stops when the test fails.
           noColor: false, // If true, protractor will not use colors in its output.
-          webdriverManagerUpdate: true,
-          args: {
-            // Arguments passed to the command
-          }
+          webdriverManagerUpdate: true
         },
-      },
-
-      protractor_webdriver: {
-        update: {
+        all: {   // Grunt requires at least one target to run so you can simply put 'all: {}' here too.
           options: {
-            path: 'node_modules/protractor/bin/',
-            command: 'webdriver-manager update',
+            configFile: "src/test/resources/protractor.conf.js", // Target-specific config file
+            args: {} // Target-specific arguments
           }
-        },
-        start: {
-          options: {
-            path: 'node_modules/protractor/bin/',
-            command: 'webdriver-manager start',
-          }
-        },
-      },
-
-
+        }
+      }
     });
 
     /**
